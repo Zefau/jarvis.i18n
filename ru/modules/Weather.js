006eca3272b2e7112f4387c198615e97560a324f
ALomonosov <64949805+ALomonosov@users.noreply.github.com>
2022-02-27 16:50:03 +0300

    Перевод на русский язык

--- ru/modules/Weather.js
+++ ru/modules/Weather.js
@@ -1,38 +1,38 @@
 export default {
-	"Weather#description": "", // Displays the weather forecast (based on www.daswetter.com).
+	"Weather#description": "Отображает прогноз погоды (используются данные с www.daswetter.com)", // Displays the weather forecast (based on www.daswetter.com).
 	
 	// Config
-	"config#Weather#apikey#label": "", // Your API key (affiliate_id) for www.daswetter.com
-	"config#Weather#localid#label": "", // The location (localid) for which the weather shall be shown
-	"config#Weather#forecastSunRain#label": "", // Display Sun / Rain Forecast
-	"config#Weather#forecastTemperature#label": "", // Display Temperature Forecast
-	"config#Weather#forecastWind#label": "", // Display Wind Forecast
-	"config#Weather#iconSetWeather#label": "", // Icon Set Weather
-	"config#Weather#iconSetWind#label": "", // Icon Set Wind
-	"config#Weather#display#label": "", // Display variant
-	"config#Weather#display#options#both": "", // Overview and preview
-	"config#Weather#display#options#overview": "", // only Overview
-	"config#Weather#display#options#preview": "", // only Preview
+	"config#Weather#apikey#label": "Ваш API key (affiliate_id) на www.daswetter.com", // Your API key (affiliate_id) for www.daswetter.com
+	"config#Weather#localid#label": "Место (localid), для которого отображать погоду", // The location (localid) for which the weather shall be shown
+	"config#Weather#forecastSunRain#label": "Показывать прогноз по солнцу/дождю", // Display Sun / Rain Forecast
+	"config#Weather#forecastTemperature#label": "Показывать прогноз температуры", // Display Temperature Forecast
+	"config#Weather#forecastWind#label": "Показывать прогноз ветра", // Display Wind Forecast
+	"config#Weather#iconSetWeather#label": "Пакет иконок для погоды", // Icon Set Weather
+	"config#Weather#iconSetWind#label": "Пакет иконок для ветра", // Icon Set Wind
+	"config#Weather#display#label": "Показать варианты", // Display variant
+	"config#Weather#display#options#both": "Обзор и предпросмотр", // Overview and preview
+	"config#Weather#display#options#overview": "только обзор", // only Overview
+	"config#Weather#display#options#preview": "только предпросмотр", // only Preview
 	
 	// Components
-	"Weather Forecast": "",
-	"today": "",
-	"tomorrow": "",
-	"TDY": "",
-	"TMRW": "",
-	"Weather": "",
-	"Rain": "",
-	"Wind": "",
-	"Pressure": "",
-	"Snowline": "",
-	"Feels like": "",
+	"Weather Forecast": "Прогноз погоды",
+	"today": "сегодня",
+	"tomorrow": "завтра",
+	"TDY": "СЕГ",
+	"TMRW": "ЗАВТ",
+	"Weather": "Погода",
+	"Rain": "Дождь",
+	"Wind": "Ветер",
+	"Pressure": "Давление",
+	"Snowline": "Снеговая линия",
+	"Feels like": "Чувствуется как",
 	
-	"WindDirection#N": "", // North
-	"WindDirection#NE": "", // Northeast
-	"WindDirection#NW": "", // Northwest
-	"WindDirection#S": "", // South
-	"WindDirection#SE": "", // Southeast
-	"WindDirection#SW": "", // Southwest
-	"WindDirection#W": "", // West
-	"WindDirection#E": "" // East
+	"WindDirection#N": "Север", // North
+	"WindDirection#NE": "Северо-восток", // Northeast
+	"WindDirection#NW": "Северо-запад", // Northwest
+	"WindDirection#S": "Юг", // South
+	"WindDirection#SE": "Юго-восток", // Southeast
+	"WindDirection#SW": "Юго-запад", // Southwest
+	"WindDirection#W": "Запад", // West
+	"WindDirection#E": "Восток" // East
 }
